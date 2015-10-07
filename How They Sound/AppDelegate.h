20e1ada6eb0f7692fdd44c5b607d3d2aa5f31699
Naveen Katari <naveen.katari@sourcebits.com>
2015-10-07 11:03:36 +0530

    Initial Commit

---
+++ How They Sound/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  How They Sound
+//
+//  Created by Naveen Katari on 07/10/15.
+//  Copyright (c) 2015 Sourcebits. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ How They Sound/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  How They Sound
+//
+//  Created by Naveen Katari on 07/10/15.
+//  Copyright (c) 2015 Sourcebits. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
